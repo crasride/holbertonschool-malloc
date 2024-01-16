8e1d35eabd8c9db295a48166f800bd77118fc888
crasride <jose.dfernandez@yahoo.fr>
2024-01-16 11:59:03 +0100

    3 define

--- naive_malloc.c
+++ naive_malloc.c
@@ -1,5 +1,6 @@
 #include "malloc.h"
 
+
 #define ALIGN(size) (((size) + 7) / 8 * 8)
 #define ALIGN_PTR(ptr) ((void *)(((size_t)(ptr) + 7) / 8 * 8))
 
@@ -7,13 +8,13 @@ void *naive_malloc(size_t size)
 {
 	size_t *chunk;
 	void *memory;
-	static void *last_break;
+	static size_t last_break;
 
 	/* Align size */
 	size = ALIGN(size) + sizeof(size_t);
 
 	/* If last break is NULL or not enough space, get more space */
-	if (last_break == NULL || (size_t)sbrk(0) - (size_t)last_break < size)
+	if (last_break == 0 || (size_t)sbrk(0) - last_break < size)
 	{
 		size_t page_size = getpagesize();
 		size_t break_size = ((size_t)sbrk(0) + page_size - 1) / page_size * page_size;
@@ -21,21 +22,22 @@ void *naive_malloc(size_t size)
 		if ((size_t)last_break % page_size + sizeof(size_t) > page_size - size)
 		{
 			sbrk(page_size - (break_size - (size_t)sbrk(0)) + size);
+			last_break = (size_t)sbrk(0) - size;
 		}
 		else
 		{
 			sbrk(size);
+			last_break = (size_t)sbrk(0) - size;
 		}
-
-		last_break = sbrk(0);
 	}
 
-	memory = ALIGN_PTR(last_break);
-	last_break = (void *)((char *)memory + size);
+	memory = ALIGN_PTR((void *)last_break);
+	last_break = (size_t)((char *)memory + size);
 
 	chunk = (size_t *)memory;
 	*chunk = size;
 
 	return ((void *)(chunk + 1));
 }
+
 

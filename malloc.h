6d67ed61c1241f07674a391d85ef6d3b66162589
guenoel <guenooel@hotmail.com>
2024-01-17 01:42:01 -0800

    small changes

---
+++ malloc.h
@@ -0,0 +1,13 @@
+#ifndef MALLOC_H
+#define MALLOC_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+/* Functions Task 0 */
+void *naive_malloc(size_t size);
+/* Functions Task 1 */
+/* Functions Task 2 */
+#endif /* MALLOC_H */

fa4b5004669efacafe4a566af70f850d31a7c5d2
crasride <jose.dfernandez@yahoo.fr>
2024-01-17 11:28:48 +0100

    add comment code

--- calloc.c
+++ calloc.c
@@ -8,20 +8,26 @@
 */
 void *_calloc(size_t nmemb, size_t size)
 {
+	/* Calculate the total size of the requested memory */
 	size_t total_size = nmemb * size;
+	/* Pointer to the allocated memory */
 	void *ptr;
 
+	/* If the requested memory is 0, return NULL */
 	if (nmemb == 0 || size == 0 || total_size / nmemb != size)
 	{
 		return (NULL);
 	}
 
+	/* Allocate memory */
 	ptr = _malloc(total_size);
 
+	/* If the allocation fails, return NULL */
 	if (ptr != NULL)
 	{
 		memset(ptr, 0, total_size);
 	}
 
+	/* Return a pointer to the allocated memory */
 	return (ptr);
 }
